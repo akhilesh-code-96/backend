5513ab8839fabc3177ab8e3235625d1756f2c6e3
akhilesh-code-96 <srivastavakhilesh157@gmail.com>
2024-05-26 13:11:19 +0530

    index

--- src/db/index.js
+++ src/db/index.js
@@ -1,15 +1,18 @@
 import mongoose from "mongoose";
-import {DB_NAME} from "../constants.js";
+import { DB_NAME } from "../constants.js";
 
-
-const connectDB = async() => {
-    try {
-        const connecctionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-        console.log(`\n MongoDB connected !! DB HOST: ${connecctionInstance.connection.host}`);
-    } catch (error) {
-        console.log("Mongo connection error: ", error);
-        process.exit(1);
-    }
-}
+const connectDB = async () => {
+  try {
+    const connecctionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+    console.log(
+      `\n MongoDB connected !! DB HOST: ${connecctionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log("Mongo connection error: ", error);
+    process.exit(1);
+  }
+};
 
 export default connectDB;
